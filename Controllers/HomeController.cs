8e0834f6d985b404b2e7ac1a264a1e0ade2345c7
Ryan <moro@ualberta.ca>
2021-10-27 09:43:18 -0700

    initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using part1.Models;
+using System.Web;
+using Microsoft.AspNetCore.Http;
+
+namespace part1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            string ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+            ViewData["ip"] = ip;
+            string cookie = Request.Cookies[ip];
+            if(cookie == null)
+            {
+                ViewData["visits"] = 1;
+                CookieOptions expiry = new CookieOptions();
+                expiry.Expires = DateTime.Now.AddYears(2);
+                Response.Cookies.Append(ip, "1", expiry);
+
+            }
+            else
+            {
+                int visits = Int32.Parse(cookie);
+                visits = visits + 1;
+                Response.Cookies.Append(ip, visits.ToString());
+                ViewData["visits"] = visits;
+            }
+
+
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ obj/Release/netcoreapp3.1/PubTmp/Out/wwwroot/js/site.js
@@ -0,0 +1,15 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+var offset = new Date().getTimezoneOffset();
+var timezone = document.getElementById("timezone")
+if (Math.sign(offset) == 1) {
+    timezone.innerHTML += "+" + offset / 60;
+}
+else if (Math.sign(offset) == -1) {
+    timezone.innerHTML += "-" + offset / 60;
+}
+
+
